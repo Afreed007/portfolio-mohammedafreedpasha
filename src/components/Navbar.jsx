9d8fee7e915e9492a3b55276b60c1421b995793d
Afreed007 <afreed0076@gmail.com>
2024-07-14 16:31:54 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/kevinRushLogo.png"
+
+const Navbar = () => {
+  return (
+    <nav className="bg-red-400 mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center font-bold ">
+            Afreeds Codes
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
