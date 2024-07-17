1505c13afee6466469698f2e7e16f600a37b9949
Afreed007 <afreed0076@gmail.com>
2024-07-17 16:59:23 +0530

    added about,hero,experience,tenchnologies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
-import logo from "../assets/kevinRushLogo.png"
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-400 mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center font-bold ">
-            Afreeds Codes
+    <nav className="mb-20 flex items-center justify-between py-6">
+        <div className="flex items-center font-light">
+            Afreed codes
+        </div>
+        
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+          <FaLinkedin />
+          <FaGithub />
+          <FaInstagram />
         </div>
     </nav>
   )
