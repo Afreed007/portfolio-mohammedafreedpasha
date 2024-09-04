7bee2fbaaae5550ddf4ab0c94d9737b46af8cc81
Afreed007 <afreed0076@gmail.com>
2024-09-04 17:01:26 +0530

    added technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,11 +1,16 @@
+import { motion } from "framer-motion"
+
 import { RiReactjsLine } from "react-icons/ri";
 import { FaJsSquare } from 'react-icons/fa';
 import {FaNodeJs } from "react-icons/fa";
 import { SiTailwindcss } from 'react-icons/si';
+import { SiPostman } from "react-icons/si";  
 // import { FaJava } from 'react-icons/fa';
 import { SiMysql } from 'react-icons/si';
+import { DiHtml5 } from "react-icons/di";
+import { DiCss3 } from "react-icons/di";
 
-import { motion } from "framer-motion"
+
 
 
 const iconVariants = (duration) =>({
@@ -21,17 +26,32 @@ const iconVariants = (duration) =>({
     }
 });
 
+
 const Technologies = () =>{
     return (
         <div className="border-b border-neutral-800 pb-24">
             <h1 className="my-20 text-center text-4xl">Technologies</h1>
             <div className="flex flex-wrap items-center justify-center gap-4">
-                <motion.div 
-                    variants={iconVariants(2.4)}
+            <motion.div 
+                    variants={iconVariants(1.4)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <RiReactjsLine className="text-7xl text-cyan-400" />
+                    <DiHtml5 className="text-7xl text-orange-400" />
+                </motion.div>
+                <motion.div 
+                    variants={iconVariants(4.5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <DiCss3 className="text-7xl text-blue-400" />
+                </motion.div>
+                <motion.div 
+                    variants={iconVariants(2)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiTailwindcss  className="text-7xl text-cyan-400" />
                 </motion.div>
                 <motion.div 
                     variants={iconVariants(3)}
@@ -48,12 +68,21 @@ const Technologies = () =>{
                     <FaNodeJs className="text-7xl text-green-500" />
                 </motion.div>
                 <motion.div 
-                    variants={iconVariants(2)}
+                    variants={iconVariants(2.4)}
                     initial="initial"
                     animate="animate"
                     className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <SiTailwindcss  className="text-7xl text-cyan-400" />
+                    <RiReactjsLine className="text-7xl text-cyan-400" />
                 </motion.div>
+                
+                <motion.div 
+                    variants={iconVariants(2.4)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiPostman className="text-7xl text-orange-400" />
+                </motion.div>
+                
                 <motion.div 
                     variants={iconVariants(6)}
                     initial="initial"
@@ -62,8 +91,7 @@ const Technologies = () =>{
                     <SiMysql className="text-7xl text-red-700" />
                 </motion.div>
                 
-                
-
+               
             </div>
         </div>
     );
