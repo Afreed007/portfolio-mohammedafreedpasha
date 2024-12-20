3c0f6706d11d50b6ebacdcbf6f163c816f6b9135
Afreed007 <afreed0076@gmail.com>
2024-12-20 20:12:56 +0530

    updated experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,34 @@
 import {EXPERIENCES} from "../constants";
+import { EDUCATION } from "../constants";
 import { motion } from "framer-motion"
+
 
 const Experience = () =>{
     return(<div className="border-b border-neutral-800 pb-4">
         <h1 className="my-20 text-center text-4xl">Experience</h1>
+
         <div>
+            {EDUCATION.map((education,index)=>(
+                <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+                    <motion.div 
+                        whileInView={{opacity:1,x:0}}
+                        initial={{opacity:0, x:-100}}
+                        transition={{duration:1}}
+                        className="w-full lg:w-1/4">
+                    <p className="mb-2 text-sm text-neutral-400">{education.year}</p>
+                    </motion.div>
+                    <motion.div 
+                        whileInView={{opacity:1,x:0}}
+                        initial={{opacity:0, x:100}}
+                        transition={{duration:1}}
+                        className="w-full max-w-xl lg:3/4">
+                        <h6 className="mb-2 font-semibold">{education.degree}</h6>
+                        <p className="mb-4 text-neutral-400">{education.institute}</p>
+                        </motion.div>
+                
+                </div>
+        
+            ))}
             {EXPERIENCES.map((experience, index)=>(
                 <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
                     <motion.div 
@@ -35,6 +59,7 @@ const Experience = () =>{
                 </div>
             ))}    
         </div> 
+
     </div>
     );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -8,10 +8,42 @@ export const HERO_CONTENT = `I am a passionate full stack developer with a knack
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 6 Months of experience, I have worked with a variety of technologies, including React, Node.js, MySQL. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
+export const EDUCATION =[
+  {
+    id:1,
+    date: "Dec 2024 - present",
+    title: "MCA(Master in computer Applications)",
+    institute: "Ramaiah Institute of Technology",
+    description: `Currently pursuing Master in Computer Applications and Engineering, where I am actively engaged in learning and gaining hands-on experience in the field. I am excited about the opportunities and challenges that lie ahead in this dynamic field.`,
+    icon:"school",
+    buttonText:"view",
+  },
+  {
+    id:2,
+    date: "Jun 2024 - Sep 2024",
+    title: "Intern as Full Stack Developer",
+    institute: "LueurTech Software Solutions Pvt.Ltd",
+    description: `Designed and developed user interfaces for web applications using SpringBoot and React. Worked closely with backend developers to integrate frontend components with SpringBoot APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["Javascript", "React.js", "SpringBoot", "MySQL"],
+    icon:"school",
+    buttonText:"view",
+  },
+  {
+    id:3,
+    date: "Oct 2021 -Sep 2024",
+    title: "BCA(Bachelor of Computer Science)",
+    institute: "Cambridge College",
+    description: `Completed my BCA with strong foundation in various subjects. I achieved grade point average of 8.4 CGPA`,
+    icon:"school",
+    buttonText:"view",
+    // technologies: ["Java", "C", "Python", "HTML", "CSS", "JavaScript"],
+  }
+  
+];
 
 export const EXPERIENCES = [
   {
-    year: "jun 2024 - Sep 2024",
+    year: "Jun 2024 - Sep 2024",
     role: "Intern as Full Stack Developer",
     company: "LueurTech Software Solutions Pvt.Ltd",
     description: `Designed and developed user interfaces for web applications using SpringBoot and React. Worked closely with backend developers to integrate frontend components with SpringBoot APIs. Implemented responsive designs and optimized frontend performance.`,
